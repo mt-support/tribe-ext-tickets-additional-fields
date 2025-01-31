930510a8ff225a7f3106f0aeefe5ee70d954771f
Andras Guseo <andras@theeventscalendar.com>
2025-01-31 11:01:50 +0100

    code formatting

--- src/admin-views/editor/input-url.php
+++ src/admin-views/editor/input-url.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * @var WP_Post $post
- * @var array  $field_data  The field data.
- * @var string $field_id    The field ID.
- * @var string $field_value The field value.
+ * @var array   $field_data  The field data.
+ * @var string  $field_id    The field ID.
+ * @var string  $field_value The field value.
  */
 
 // Don't load directly.
@@ -12,22 +12,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<?php if ( ! empty( $field_data[ 'label' ] ) ) : ?>
+<?php if ( ! empty( $field_data['label'] ) ) : ?>
 	<label for="<?php echo esc_attr( $field_id ); ?>" class="ticket_form_label ticket_form_left">
-		<?php echo esc_html( $field_data[ 'label' ] ); ?>
+		<?php echo esc_html( $field_data['label'] ); ?>
 	</label>
 <?php endif; ?>
 
-<input
-	type="url"
-	id="<?php echo esc_attr( $field_id ); ?>"
-	name="<?php echo esc_attr( $field_id ); ?>"
-	class="ticket_field ticket_form_right"
-	value="<?php echo esc_attr( $field_value ); ?>"
->
+	<input
+			type="url"
+			id="<?php echo esc_attr( $field_id ); ?>"
+			name="<?php echo esc_attr( $field_id ); ?>"
+			class="ticket_field ticket_form_right"
+			value="<?php echo esc_attr( $field_value ); ?>"
+	>
 
-<?php if ( ! empty( $field_data[ 'description' ] ) ) : ?>
+<?php if ( ! empty( $field_data['description'] ) ) : ?>
 	<p class="description ticket_form_right">
-		<?php echo esc_html( $field_data[ 'description' ] ); ?>
+		<?php echo esc_html( $field_data['description'] ); ?>
 	</p>
 <?php endif; ?>
